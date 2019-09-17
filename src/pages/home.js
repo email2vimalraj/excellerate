f5dabe15db79a2393f4fe72c42887c6f28bd060e
Vimalraj Selvam <email2vimalraj@gmail.com>
2019-09-17 23:11:34 +0800

    Fixed the button stack on the home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,23 +4,25 @@ import { Row, Col, Button } from "antd";
 import "../style.css";
 
 export default () => (
-  <Row type="flex" justify="center" style={{ marginTop: 80, marginBottom: 80 }}>
-    {/* <Row justify="space-between" type="flex"> */}
-    <Col lg={4} sm={2} md={6}>
-      <Button size="large" className="button" href="/question">
-        Q & A Session
-      </Button>
-    </Col>
-    <Col lg={4} sm={2} md={6}>
-      <Button size="large" className="button" href="/feedback">
-        Let's Connect!
-      </Button>
-    </Col>
-    <Col lg={4} sm={2} md={6}>
-      <Button size="large" className="button" href="/poll">
-        Poll
-      </Button>
-    </Col>
-    {/* </Row> */}
-  </Row>
+  <>
+    <Row type="flex" justify="center" style={{ marginTop: 80 }} gutter={48}>
+      <Col lg={8} md={10} sm={20} xs={20} className="home-container">
+        <Button size="large" block className="button" href="/question">
+          Live Q & A
+        </Button>
+      </Col>
+      <Col lg={8} md={10} sm={20} xs={20} className="home-container">
+        <Button size="large" block className="button" href="/poll">
+          Live Poll
+        </Button>
+      </Col>
+    </Row>
+    <Row type="flex" justify="center" style={{ marginBottom: 80 }}>
+      <Col lg={8} md={10} sm={20} xs={20}>
+        <Button size="large" block className="button" href="/feedback">
+          Let's Connect!
+        </Button>
+      </Col>
+    </Row>
+  </>
 );
